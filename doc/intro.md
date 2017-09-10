320106b62a750b217e844dbfb77f5711b610466c
Martin Grandrath <mail@martin-grandrath.de>
2017-09-10 11:19:00 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to docker-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/docker_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns docker-test.core-test
+  (:require [clojure.test :refer :all]
+            [docker-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
